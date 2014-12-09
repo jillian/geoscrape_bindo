5bd6938f87521ee6ea263d1eb3a7ed8bd10f3d2d
jmac84242 <jillian.macnaughton@gmail.com>
2014-12-09 07:59:16 +0100

    added manual grid .. need to fix grid code

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -9,15 +9,22 @@ class MainController < ApplicationController
     pr_id = ParentRequestId.new
     request_id = pr_id.get_id
 
-    # geo_grid.each do |bound|
+    manual_grid = [
+      [37.746964676, 122.453784943, 37.7876744001, 122.402629852],
+      [33.746964676, 118.453784943, 33.7876744001, 118.402629852],
+      [33.746964676, 118.402629852, 33.7876744001, 118.352629852],
+      [33.746964676, 118.352629852, 33.7876744001, 118.302629852]
+    ]
+
+    manual_grid.each do |bound|
       sleep 10
-      bound = [33.746964676, 118.453784943, 33.7876744001, 118.402629852]
+      #bound = [33.746964676, 118.453784943, 33.7876744001, 118.402629852]
       categories.each do |category|
         sleep 10
         crawler = YelpWorker.new
         crawler.perform(bound, category.url_name, request_id)
       end
     # redirect_to :root_url
-    # end
+    end
   end
 end
--- app/services/grid.rb
+++ app/services/grid.rb
@@ -1,24 +1,31 @@
 class Grid
+
+# LA Grid --> 
+# sw_latitude: 33.746964676
+# sw_longitude: -118.453784943
+# ne_latitude: 34.146964676
+# ne_longitude: -118.039200000
+
   def build
     grid_array = []
-    original_grid_block = [33.818300.to_f, 118.500300.to_f, 33.818800.to_f, 118.499800.to_f]
+    original_grid_block = [33.818300.to_f, 118.500300.to_f, 33.833800.to_f, 118.499800.to_f]
     grid_array << original_grid_block
-    #.0005 at 47 degrees latitude ~ 0.5 km. Making .5 km^2 grids
+    
 
    
     grid = original_grid_block
-    until grid[2] >= 34.159200.to_f
-      until grid[3] <= 118.039200.to_f
+    until grid[2] >= 34.146964676.to_f
+      until grid[3] <= 118.039200000.to_f
         block = []
         block[0] = grid[0]
         block[1] = grid[3].round(6)
         block[2] = grid[2]
-        block[3] = (grid[3] - 0.000500.to_f).round(6)
+        block[3] = (grid[3] - 0.020000.to_f).round(9)
         grid_array << block
         grid = block
       end
-      grid[2] = (grid[2] + 0.000500).round(6)
-      grid[0] = (grid[0] + 0.000500).round(6)
+      grid[0] = grid[2]
+      grid[2] = (grid[2] + 0.020000).round(9)
       grid[1] = original_grid_block[1]
       grid[3] = original_grid_block[3]
     end
@@ -26,10 +33,15 @@ class Grid
   end
 end
 
-#http://www.yelp.com/search/snippet?find_desc=publicservicesgovt&find_loc=&l=g%3A
 
-# san fran grid
+# san fran grid block
 # -122.453784943 difference: .051155091
 # -122.402629852
-#  37.746964676  difference: .0407097241
-#  37.7876744001
+#  37.746964676  difference: .0407097241 distance between: 2.814 mi, 4.528km
+#  37.7876744001  .02 diff => 1.432 mi, 2.304 km
+
+# LA grid block
+# -118.453784943
+# -118.402629852
+# 33.746964676
+# 33.7876744001 
