b1ed6fc9608c38dcc4d61724939bb267dd1b6e28
jmac84242 <jillian.macnaughton@gmail.com>
2014-12-09 22:24:11 +0100

    made grid more easily mutable for future options

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,23 +3,20 @@ require 'rest_client'
 class MainController < ApplicationController
 
   def scrape
+
+    #customize these for another city/region/area
+    geo_area_bounds = {
+      sw_lat: 33.74, 
+      sw_lng: 118.45, 
+      ne_lat: 34.20, 
+      ne_lng: 118.00
+    }
+
     grid = Grid.new
-    geo_grid = grid.build
+    geo_grid = grid.build(geo_area_bounds)
     categories = Category.all
     pr_id = ParentRequestId.new
     request_id = pr_id.get_id
-
-    # manual_grid = [
-    #   # [34.037000000, 118.208000000, 34.0770000000, 118.208000000]
-    #   # [37.746964676, 122.453784943, 37.7876744001, 122.402629852],
-    #   # [33.746964676, 118.453784943, 33.7876744001, 118.402629852],
-    #   # [33.746964676, 118.402629852, 33.7876744001, 118.352629852],
-    #   # [33.746964676, 118.352629852, 33.7876744001, 118.302629852],
-    #   # [33.746964676, 118.302629852, 33.7876744001, 118.252629852],
-    #   # [33.746964676, 118.252629852, 33.7876744001, 118.202629852],
-    #   # [33.787674400, 118.252629852, 33.8276744001, 118.202629852],
-    #   [33.827674400, 118.252629852, 33.8676744001, 118.202629852]
-    # ]
 
     geo_grid.each do |bound|
       sleep 10
--- app/services/grid.rb
+++ app/services/grid.rb
@@ -1,7 +1,7 @@
 class Grid
 
 # choose any grid size/location and parser should work
-geo_area_bounds = [sw_lat: 33.74, sw_lng: 118.45, ne_lat: 34.20, ne_lng: 118.00] 
+# geo_area_bounds = { :sw_lat => 33.74, :sw_lng => 118.45, :ne_lat => 34.20, :ne_lng => 118.00 } 
 
   def build(geo_area_bounds)
     #build rows 
@@ -12,14 +12,15 @@ geo_area_bounds = [sw_lat: 33.74, sw_lng: 118.45, ne_lat: 34.20, ne_lng: 118.00]
       (geo_area_bounds[:sw_lat] + 0.04), 
       (geo_area_bounds[:sw_lng] - 0.05)
     ]
+
     grid_array = []
 
     grid = original_grid
-    until grid[3] < geo_area_bounds[:ne_lng]
+    until grid[3] < geo_area_bounds[:ne_lng].to_f
       row_block = []
-      row_block[0] = original_grid[0].to_f
+      row_block[0] = original_grid[0]
       row_block[1] = grid[3]
-      row_block[2] = original_grid[2].to_f
+      row_block[2] = original_grid[2]
       row_block[3] = grid[3] - 0.05
       row_block.map do |x|
         x.to_f
@@ -50,6 +51,7 @@ geo_area_bounds = [sw_lat: 33.74, sw_lng: 118.45, ne_lat: 34.20, ne_lng: 118.00]
         grid_array << column_block
       end
     end
+    grid_array
   end
 
 
